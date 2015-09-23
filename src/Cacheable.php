8d289c9a4e590e2f056217a80e98212b12078e9d
pulkit <pulkit@clowdy.com>
2015-09-23 11:53:43 +0100

    added events for removing from cache

--- src/Cacheable.php
+++ src/Cacheable.php
@@ -2,10 +2,38 @@
 
 namespace PulkitJalan\Cacheable;
 
+use Illuminate\Support\Facades\Cache;
 use PulkitJalan\Cacheable\Eloquent\Builder;
 
 trait Cacheable
 {
+    /**
+     * Register any other events for your application.
+     *
+     * @param  \Illuminate\Contracts\Events\Dispatcher  $events
+     * @return void
+     */
+    public function boot(DispatcherContract $events)
+    {
+        parent::boot($events);
+
+        static::updated(function ($model) {
+            Cache::tag($this->getTable())->forget($model->{$model->getKeyName()});
+        }, -1);
+
+        static::saved(function ($model) {
+            Cache::tag($this->getTable())->forget($model->{$model->getKeyName()});
+        }, -1);
+
+        static::deleted(function ($model) {
+            Cache::tag($this->getTable())->forget($model->{$model->getKeyName()});
+        }, -1);
+
+        static::restored(function ($model) {
+            Cache::tag($this->getTable())->forget($model->{$model->getKeyName()});
+        }, -1);
+    }
+
     /**
      * Create a new Eloquent query builder for the model.
      *
